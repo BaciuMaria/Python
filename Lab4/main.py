a1a6e4d9649c3ea744f8241bfae895d251bfd72e
BaciuMaria <100144074+BaciuMaria@users.noreply.github.com>
2023-11-05 21:53:58 +0200

    + ex 3

--- Lab4/main.py
+++ Lab4/main.py
@@ -97,3 +97,89 @@ print(queue.pop())
 print("")
 
 # Problema 3
+
+class Matrix:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.cell = [[0 for _ in range(m)] for _ in range(n)]
+
+    def get(self, i, j):
+        if 0 <= i < self.n and 0 <= j < self.m:
+            return self.cell[i][j]
+        else:
+            return None
+
+    def set(self, i, j, value):
+        if 0 <= i < self.n and 0 <= j < self.m:
+            self.cell[i][j] = value
+
+    def transpose(self):
+        transposed = Matrix(self.m, self.n)
+        for i in range(self.n):
+            for j in range(self.m):
+                transposed.cell[j][i] = self.cell[i][j]
+        return transposed
+
+    def multiply(self, other_matrix):
+        if self.m != other_matrix.n:
+            return None
+        result = Matrix(self.n, other_matrix.m)
+        for i in range(self.n):
+            for j in range(other_matrix.m):
+                for k in range(self.m):
+                    result.cell[i][j] += self.cell[i][k] * other_matrix.cell[k][j]
+        return result
+
+    def function(self, transformation):
+        for i in range(self.n):
+            for j in range(self.m):
+                self.cell[i][j] = transformation(self.cell[i][j])
+
+    def print(self):
+        for row in self.cell:
+            print(row)
+
+print("Problema 3")
+print("")
+
+matrix1 = Matrix(2, 4)
+matrix1.set(0, 0, 1)
+matrix1.set(0, 1, 2)
+matrix1.set(0, 2, 3)
+matrix1.set(0, 3, 4)
+matrix1.set(1, 0, 5)
+matrix1.set(1, 1, 6)
+matrix1.set(1, 2, 7)
+matrix1.set(1, 3, 8)
+
+matrix2 = Matrix(4, 2)
+matrix2.set(0, 0, 9)
+matrix2.set(0, 1, 10)
+matrix2.set(1, 0, 11)
+matrix2.set(1, 1, 12)
+matrix2.set(2, 0, 13)
+matrix2.set(2, 1, 14)
+matrix2.set(3, 0, 15)
+matrix2.set(3, 1, 16)
+
+print("Matrix 1:")
+matrix1.print()
+
+print("Matrix 2:")
+matrix2.print()
+
+result = matrix1.multiply(matrix2)
+print("Multiplication:")
+if result is None:
+    print("The matrixes have different dimensions.")
+else :
+    result.print()
+
+transposed = matrix1.transpose()
+print("Transpose:")
+transposed.print()
+
+matrix1.function(lambda x: x + 5)
+print("Transformation:")
+matrix1.print()
